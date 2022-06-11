11ce1ff35a4b9bf6fd8126b670c51d1e3d15734f
AndrePousa <andre.roberto.pousa@gmail.com>
2022-06-10 23:27:40 -0300

    Login2

--- frontend/src/helpers/AuthHandler.js
+++ frontend/src/helpers/AuthHandler.js
@@ -6,3 +6,12 @@ export const isLogged = () =>{
   let token = Cookies.get('token');
   return (token)? true : false;
 }
+
+export const doLogin = (token, rememberPassWord = false) => {
+  if(rememberPassWord){
+    Cookies.set('token', token, {expires:999})
+  }
+  else{
+    Cookies.set('token', token);
+  }
+}
