3803b8bb5e7a5fffab1a7e381db646e3bc3345df
AndrePousa <andre.roberto.pousa@gmail.com>
2022-08-31 12:16:36 -0300

    Atualização do Git SSH

--- frontend/src/pages/signin/index.js
+++ frontend/src/pages/signin/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { PageContainer, PageTitle } from '../../components/MainComponents';
+import { PageContainer, PageTitle, ErrorMessage } from '../../components/MainComponents';
 import { PageArea } from './styled';
 import useApi from '../../helpers/OlxApi';
 import { doLogin } from '../../helpers/AuthHandler';
@@ -32,23 +32,43 @@ const Signin = () =>{
      <PageContainer>
       <PageTitle>Login</PageTitle>
       <PageArea>
+        {error &&
+          <ErrorMessage>{error}</ErrorMessage>
+        }
         <form onSubmit={handleSubmit}>
           <label className="area">
             <div className="area--title">E-mail</div>
             <div className="area--input">
-              <input type="email" disabled={disabled} />
+              <input 
+                type="email" 
+                disabled={disabled} 
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
             </div>
           </label>
           <label className="area">
             <div className="area--title">Senha</div>
             <div className="area--input">
-              <input type="password" disabled={disabled} />
+              <input 
+                type="password" 
+                disabled={disabled} 
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                required
+              />
             </div>
           </label>
           <label className="area">
             <div className="area--title">Lembrar Senha</div>
             <div className="area--input">
-              <input type="checkbox" disabled={disabled} />
+              <input 
+                type="checkbox" 
+                disabled={disabled}
+                cheked={rememberPasword} 
+                onChange={() => setRememberPasword(!rememberPasword)}
+              />
             </div>
           </label>
           <label className="area">
